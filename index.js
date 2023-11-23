f96db0011ae14ecfd3249ebd535f24732095b55f
Yog4Prog <raju4techno@gmail.com>
2023-11-23 18:32:14 +0530

    Updated

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+var myModule = require('./myModule');
+
 
 try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
+  myModule.sayHello(); // logs "Hello, World!"
   console.log(`Hello ${nameToGreet}!`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
